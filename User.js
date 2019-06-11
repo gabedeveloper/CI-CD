95540d1398d8ed722b6427ca04284c4fec4e6a2a
Aleksandr Maksimenko <oleksandr.maksymenko@smartexe.com>
2019-06-11 16:06:28 +0300

    Create User class

---
+++ User.js
@@ -0,0 +1,31 @@
+class User {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  get name() {
+    return this.name;
+  }
+
+  set name(name) {
+    this.name = name;
+  }
+
+  get lastName() {
+    return this.lastName;
+  }
+
+  set lastName(lastName) {
+    this.lastName = lastName;
+  }
+
+  get age() {
+    return this.age;
+  }
+
+  set age(age) {
+    this.age = age;
+  }
+}
